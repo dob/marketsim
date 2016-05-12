9f83c9e697d8918cfb28b226a77f6a704859db6a
Doug Petkanics <petkanics@gmail.com>
2016-05-12 12:42:36 -0400

    Added flags to the main function to determine whether to run the simple or full example.

--- cmd/marketsim/examples/main.go
+++ cmd/marketsim/examples/main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"flag"
+)
+
+// An example provides a run() method so that this main
+// function can execute it depending on the command line
+// arguments.
 type Example interface {
 	run()
 }
@@ -8,9 +15,16 @@ func run(ex Example) {
 	ex.run()
 }
 
+var flagMapping = map[string]Example {
+	"simple": SimpleExample{},
+	"full": FullNasdaqSim{}}
+
+// User can pass -example=full to run the full nasdaq sim
+// otherwise it defaults to the simple example.
 func main() {
-	var ex1 FullNasdaqSim
-	var ex2 SimpleExample
-	run(ex1)
-	run(ex2)
+	var example = flag.String("example", "simple", "Pass either -example=simple or -example=full")
+	flag.Parse()
+
+	var ex = flagMapping[*example]
+	run(ex)
 }
